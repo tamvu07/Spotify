4bf74f4c6815d4ef9b385a9f0810ca2d17a61a4f
Tam Vu <tam.vm@aris-vn.com>
2021-07-27 17:34:47 +0700

    +Create project

---
+++ Spotify/Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Vu Minh Tam on 7/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
