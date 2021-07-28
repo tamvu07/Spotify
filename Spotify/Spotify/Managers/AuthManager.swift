0a9766d3b9643b60066fa2c5b1a92d3ae71db8a8
Tam Vu <tam.vm@aris-vn.com>
2021-07-28 11:07:38 +0700

    Auth Set Up +Add (https://www.youtube.com/watch?v=MfhwNT5uT2s&list=PL5PR3UyfTWve9ZC7Yws0x6EGjBO2FGr0o&index=3)

--- Spotify/Spotify/Managers/AuthManager.swift
+++ Spotify/Spotify/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "131e39bc89ba49e2b2ccd6067d04a00b"
+        static let clientSecrect = "f438ab4fde6c4742b0efc3b8524a75b2"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirecURL = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirecURL)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -20,11 +33,30 @@ final class AuthManager {
         return nil
     }
     
+    private var refreshToken: String? {
+        return nil
+    }
+    
     private var tokenExpirationDate: Date? {
         return nil
     }
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(
+        code: String,
+        competion: @escaping ((Bool) -> Void)
+    ){
+        // Get Token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    public func cacheToken() {
+        
     }
 }
