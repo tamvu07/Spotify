23e05a26024d42d4a8ed71b2a7f625a4ea006554
Tam Vu <tam.vm@aris-vn.com>
2021-07-29 09:47:07 +0700

    Part 5 +Add (https://www.youtube.com/watch?v=0OTUxEEFfZU&list=PL5PR3UyfTWve9ZC7Yws0x6EGjBO2FGr0o&index=6)

--- Spotify/Spotify/Managers/AuthManager.swift
+++ Spotify/Spotify/Managers/AuthManager.swift
@@ -132,14 +132,14 @@ final class AuthManager {
         }
     }
     
-    public func refreshIfNeeded(completion: @escaping (Bool) -> Void) {
+    public func refreshIfNeeded(completion: ((Bool) -> Void)?) {
         
         guard !refreshingToken else {
             return
         }
         
         guard shouldRefreshToken else {
-            completion(true)
+            completion?(true)
             return
         }
         
@@ -173,7 +173,7 @@ final class AuthManager {
         let data = basicToken.data(using: .utf8)
         guard let base64String = data?.base64EncodedString() else {
             print("Failure to get base64")
-            completion(false)
+            completion?(false)
             return
         }
         
@@ -184,7 +184,7 @@ final class AuthManager {
             self?.refreshingToken = false
             guard let data = data,
                   error == nil else {
-                completion(false)
+                completion?(false)
                 return
             }
             
@@ -193,10 +193,10 @@ final class AuthManager {
                 self?.onRefreshBlocks.forEach { $0(result.access_token)}
                 self?.onRefreshBlocks.removeAll()
                 self?.cacheToken(result: result)
-                completion(true)
+                completion?(true)
             } catch {
                 print(error.localizedDescription)
-                completion(false)
+                completion?(false)
             }
         }
         tast.resume()
