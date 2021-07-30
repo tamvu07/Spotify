0a44f088a02752a563f4ab8bb362712449b80180
Tam Vu <tam.vm@aris-vn.com>
2021-07-30 17:37:54 +0700

    Part 12 +Add (https://www.youtube.com/watch?v=aTrs7TsHhmg&list=PL5PR3UyfTWve9ZC7Yws0x6EGjBO2FGr0o&index=13)

---
+++ Spotify/Spotify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  Spotify
+//
+//  Created by Vu Minh Tam on 7/30/21.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
