03ba50ee6d514831b510e7d9851d1b6d617ebba5
Tam Vu <tam.vm@aris-vn.com>
2021-08-03 13:57:59 +0700

    Part 17 Search Cells +Add (https://www.youtube.com/watch?v=evPoFlS1Zxw&list=PL5PR3UyfTWve9ZC7Yws0x6EGjBO2FGr0o&index=19)

---
+++ Spotify/Spotify/ViewModels/SearchResultDefaultTableViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultDefaultTableViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Vu Minh Tam on 8/3/21.
+//
+
+import Foundation
+
+struct SearchResultDefaultTableViewCellViewModel {
+    let title: String
+    let imageURL: URL?
+}
---
+++ Spotify/Spotify/ViewModels/SearchResultSubtitleTableViewCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultSubtitleTableViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Vu Minh Tam on 8/3/21.
+//
+
+import Foundation
+
+struct  SearchResultSubtitleTableViewCellViewModel {
+    let title: String
+    let subtitle: String
+    let imageURL: URL?
+}
