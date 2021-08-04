bf9fb2d1340ef3a180c2e4b48d242b40a4c1dc7a
Tam Vu <tam.vm@aris-vn.com>
2021-08-04 13:17:51 +0700

    Part 21 Library UI +Add (https://www.youtube.com/watch?v=A3Sj8ZMVVAE&list=PL5PR3UyfTWve9ZC7Yws0x6EGjBO2FGr0o&index=22)

---
+++ Spotify/Spotify/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify
+//
+//  Created by Vu Minh Tam on 8/4/21.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ Spotify/Spotify/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  Spotify
+//
+//  Created by Vu Minh Tam on 8/4/21.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
